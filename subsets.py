cbd5c9392e5919f8a8b676deb153d4ef88b3adc5
Ashi Krishnan <me@ashi.works>
2017-10-27 12:41:24 -0400

    Subsets in python.

--- subsets.py
+++ subsets.py
@@ -12,6 +12,16 @@ def subsets(s):
     output.add(frozenset(with_first))
   return output
 
-print subsets(set([1,2,3]))
+def subsets2(s):
+  for i in xrange(2 ** len(s)):
+    yield subsetWithIndex(s, i)
+
+def subsetWithIndex(input, i):
+  return frozenset(x for (b, x) in enumerate(input) if (i >> b) & 1)
+
+s = set([1,2,3])
+print subsets(s)
+print set(subsets2(s))
+assert len(subsets(s)) == len(set(subsets2(s)))
   
   
